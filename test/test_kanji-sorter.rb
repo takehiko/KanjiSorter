ce136bb33c44beec0b9df32cff75a305338609ac
takehiko <takehiko@sys.wakayama-u.ac.jp>
2020-03-07 15:22:18 +0900

    fix test

--- test/test_kanji-sorter.rb
+++ test/test_kanji-sorter.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
-#require "minitest/unit"
 require "minitest/autorun"
 require "./kanji-sorter.rb"
 
-class TestKanjiYear < Minitest::Unit::TestCase
+class TestKanjiYear < Minitest::Test
   def test_years
     assert_equal(2017, KanjiYear.year(use: "2020-05-01"))
     assert_equal(2017, KanjiYear.year(use: "R2sy"))
@@ -35,14 +34,14 @@ class TestKanjiYear < Minitest::Unit::TestCase
 
   # 対象を切り替えて同一文字列で結果を確認する
   def test_kanji_sorter
-    seq = "大阪府兵庫県京都府滋賀良県和歌山県三重県香川県徳島県"
+    seq = "大阪府兵庫県京都府滋賀県奈良県和歌山県三重県香川県徳島県"
 
     ks2017 = KanjiSorter.new(seq, pub: "2017").analyze
     ks2017_freq_grade = ks2017.to_a
     assert_match(/三山川大/, ks2017_freq_grade[1])
     assert_match(/歌京/, ks2017_freq_grade[2])
-    assert_match(/県県県県県県庫重都島和/, ks2017_freq_grade[3])
-    assert_match(/賀香滋徳阪府府兵良/, ks2017_freq_grade[4])
+    assert_match(/県県県県県県県庫重都島和/, ks2017_freq_grade[3])
+    assert_match(/賀香滋徳奈阪府府兵良/, ks2017_freq_grade[4])
     assert_match(/0種0字/, ks2017_freq_grade[5])
     assert_match(/0種0字/, ks2017_freq_grade[6])
     assert_match(/0種0字/, ks2017_freq_grade[0])
@@ -51,30 +50,30 @@ class TestKanjiYear < Minitest::Unit::TestCase
     ks1989_freq_grade = ks1989.to_a
     assert_match(/三山川大/, ks1989_freq_grade[1])
     assert_match(/歌京/, ks1989_freq_grade[2])
-    assert_match(/県県県県県県庫重都島和/, ks1989_freq_grade[3])
+    assert_match(/県県県県県県県庫重都島和/, ks1989_freq_grade[3])
     assert_match(/府府兵良/, ks1989_freq_grade[4])
     assert_match(/賀徳/, ks1989_freq_grade[5])
     assert_match(/0種0字/, ks1989_freq_grade[6])
-    assert_match(/滋阪香/, ks1989_freq_grade[0])
+    assert_match(/奈滋阪香/, ks1989_freq_grade[0])
 
     ks1977 = KanjiSorter.new(seq, pub: "1977").analyze
     ks1977_freq_grade = ks1977.to_a
     assert_match(/三山川大/, ks1977_freq_grade[1])
     assert_match(/歌京/, ks1977_freq_grade[2])
-    assert_match(/県県県県県県庫重都島和/, ks1977_freq_grade[3])
+    assert_match(/県県県県県県県庫重都島和/, ks1977_freq_grade[3])
     assert_match(/府府兵良/, ks1977_freq_grade[4])
     assert_match(/賀徳/, ks1977_freq_grade[5])
     assert_match(/0種0字/, ks1977_freq_grade[6])
-    assert_match(/滋阪香/, ks1977_freq_grade[0])
+    assert_match(/奈滋阪香/, ks1977_freq_grade[0])
 
     ks1958 = KanjiSorter.new(seq, pub: "1958").analyze
     ks1958_freq_grade = ks1958.to_a
     assert_match(/三山川大/, ks1958_freq_grade[1])
     assert_match(/京/, ks1958_freq_grade[2])
-    assert_match(/歌県県県県県県庫重都島和/, ks1958_freq_grade[3])
+    assert_match(/歌県県県県県県県庫重都島和/, ks1958_freq_grade[3])
     assert_match(/府府良/, ks1958_freq_grade[4])
     assert_match(/賀兵/, ks1958_freq_grade[5])
     assert_match(/徳/, ks1958_freq_grade[6])
-    assert_match(/滋阪香/, ks1958_freq_grade[0])
+    assert_match(/奈滋阪香/, ks1958_freq_grade[0])
   end
 end
